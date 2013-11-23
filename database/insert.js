cb966059408a106ab7e88e95ea7ce77bacde4b91
implementer <alexandru.horsa@gmail.com>
2013-11-23 14:13:31 +0200

    Added database helper

---
+++ database/insert.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: alex
+ * Date: 11/23/13
+ * Time: 2:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ routes/dbhelper.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+exports.scores = function (req, res) {
+    http.get('http://hendeptycleystordifteric:3WUW8OoJhRVboQjXuBeHmiuK@implementer.cloudant.com/fitnessathome/_design/views/_view/sorted_scores?reduce=false&include_docs=true',function (response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        console.log('BODY: ');
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            var rows = chunk.rows;
+        });
+    }).on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+};
