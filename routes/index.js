754afe80f9e04b7ddfd7d003bb61c0da9d0b52f3
dianapaun90 <diana.paun90@gmail.com>
2013-11-23 15:43:08 +0200

    added untracked files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,23 @@
  * GET home page.
  */
 
+var http = require('http');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'FitnessChallenge' });
+  http.get('http://hendeptycleystordifteric:3WUW8OoJhRVboQjXuBeHmiuK@implementer.cloudant.com/fitnessathome/_design/views/_view/sorted_scores?reduce=false&include_docs=true',
+      function (response) {
+          console.log('STATUS: ' + response.statusCode);
+          console.log('HEADERS: ' + JSON.stringify(response.headers));
+          console.log('BODY: ');
+          response.setEncoding('utf8');
+          response.on('data', function (chunk) {
+              console.log(chunk);
+              var jsonData = JSON.parse(chunk);
+              var rows = jsonData.rows;
+              res.render('index', { title: 'FitnessChallenge', data: rows });
+          });
+      }).on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
+
 };
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,5 +1,19 @@
 var dbHelper = require('dbhelper');
 
 exports.leaderboard = function(req, res) {
-    res.render('leaderboard');
+    function callback(response) {
+        console.log('STATUS: ' + response.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(response.headers));
+        console.log('BODY: ');
+        response.setEncoding('utf8');
+        response.on('data', function (chunk) {
+            console.log(chunk);
+            var rows = chunk.rows;
+        });
+        res.render('leaderboard');
+    }
+//    .on('error', function(e) {
+//        console.log('problem with request: ' + e.message);
+//    });
+    dbHelper.scores(callback);
 }
